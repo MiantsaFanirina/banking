5af8a0773505db08742cc2505e1136e958054651
MiantsaFanirina <miantsafanirinarakotondrafara@gmail.com>
2024-05-21 17:11:28 +0300

    Home Header Layout creation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
+// components
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+
 const Home = () => {
+  const LoggedIn = {firstName: 'Miantsa Fanirina'}
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className='home-content'>
+        <header className="home-header">
+
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={LoggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+
+        </header>
+      </div>
+    </section>
   )
 }
 
